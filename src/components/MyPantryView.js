b56102fbf68f885e5b1c20fb660c43b925a24497
BenHardin <bbhardin1@gmail.com>
2021-09-30 11:19:56 -0400

    Updates pantry page to a functional component

--- src/components/MyPantryView.js
+++ src/components/MyPantryView.js
@@ -1,29 +1,70 @@
 import React from "react";
-import GenerateTableView from "./GenerateTableView";
-import App from "./App";
+import BootstrapTable from 'react-bootstrap-table-next';
+import { Button } from 'react-bootstrap'
+import firebase, {auth} from "../firebase";
 
-class MyPantryView extends React.Component {
 
-    test() {
-        return App()
+
+export default function MyPantryView() {
+
+    async function getIngredients() {
+
+        try {
+            let userId = auth.tenantId;
+            console.log(userId);
+            firebase.firestore().collection("Users")
+        } catch (err) {
+            console.log(err.message)
+        }
+
     }
 
-    render() {
-        return (
-            <div>
-                <div className='contentInsets'>
-                    <div className='pageTitle'>
-                        My Pantry
-                        <span style={{padding: 10}} /* This is blocking the Nav Bar Buttons */ />
-                        <button>+</button>
-                    </div>
-                    <div>
-                        <GenerateTableView/>
-                    </div>
+    const products = [
+        { id: "Chicken", name: "1 week", delete: <Button>Trash Icon {getIngredients}</Button> },
+
+    ];
+
+    const columns = [
+        {
+            dataField: "id",
+            text: "Ingredient",
+            sort: true
+        },
+        {
+            dataField: "name",
+            text: "Expires in",
+            sort: true
+        },
+        {
+            dataField: "delete",
+            text: "Delete"
+        }
+    ];
+
+    const selectRow = {
+        mode: 'radio', // single row selection
+        clickToSelect: true,
+        selectColumnPosition: 'right'
+    };
+
+    return (
+        <div className='MyPantryView'>
+            <div className='contentInsets'>
+                <div className='pageTitle'>
+                    My Pantry
+                    <span style={{padding: 10}} /* This is blocking the Nav Bar Buttons */ />
+                    <button>+</button>
+                </div>
+                <div>
+                    <BootstrapTable
+                        bootstrap4
+                        keyField="id"
+                        data={products}
+                        columns={columns}
+                    />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-export default MyPantryView;
