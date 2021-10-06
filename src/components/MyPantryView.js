e10fc16ac8892a0f388b80f762b9ca4539bccb4e
BenHardin <bbhardin1@gmail.com>
2021-10-06 11:37:02 -0400

    Gets pantry ingredients from database

--- src/components/MyPantryView.js
+++ src/components/MyPantryView.js
@@ -1,45 +1,92 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 import { Button } from 'react-bootstrap';
 import { BsFillTrashFill } from 'react-icons/bs';
-import firebase, {auth} from "../firebase";
+import firebase from "../firebase";
+import {useAuth} from "../contexts/AuthContext";
+import { doc, getDoc, collection, query, getDocs } from "firebase/firestore";
+import {getFirestore} from "firebase/firestore";
+import {initializeApp} from "firebase/app";
 
 
 
 export default function MyPantryView() {
 
     const [error, setError] = useState("");
+    const {uid} = useAuth();
+    const [ingredientNames, setIngredientNames] = useState([]);
+    const [ingredientExpirations, setIngredientExpirations] = useState([]);
+
+    useEffect(() => {
+        getIngredients();
+    }, []);
 
     async function getIngredients() {
 
+        // if (!uid) {
+        //     setError("User not logged in.")
+        //     return;
+        // }
+
         try {
-            let userId = auth.tenantId;
-            console.log(userId);
-            firebase.firestore().collection("Users").doc(userId).collection("Pantry").get()
+            // TODO: Move from here
+
+            const firebaseConfig = {
+                apiKey: "AIzaSyCqsDGSsqBVbApAf86ypvwLxP0qAmgH1-I",
+                authDomain: "appetyte-7a6f6.firebaseapp.com",
+                projectId: "appetyte-7a6f6",
+                storageBucket: "appetyte-7a6f6.appspot.com",
+                messagingSenderId: "470203778412",
+                appId: "1:470203778412:web:5a5f9976d8081213a01f49",
+                measurementId: "G-E8RDH36LL3"
+            };
+
+            const app = initializeApp(firebaseConfig);
+            const db = getFirestore(app);
+
+            // TODO: GET USER ID
+            const pantryRef = collection(db, "Users", "I003q2hhTK4RT80uW5AA", "Pantry")
+            const q = query(pantryRef);
+            const querySnapshot = await getDocs(q);
+            let ingredients = []
+            let expirations = []
+            let index = 0;
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                ingredients.push({id: index, name: doc.id, expiration: doc.data()["expiration"].toString(), delete: <BsFillTrashFill onClick={(event) => deleteIngredient(event, index)}/>});
+                index++;
+            });
+
+            setIngredientNames(ingredients);
+            setIngredientExpirations(expirations);
+
+            //setError(ingredients.to);
+
+            // db.collection("Users").doc("I003q2hhTK4RT80uW5AA").collection("Pantry").get().then((result) => {
+            //     setError("hello")
+            // })
         } catch (err) {
-            console.log(err.message)
+            setError(err.message);
+            console.log(err.message);
         }
 
     }
 
-    const [products, setProducts] = useState([]);
-
-    function setIngredients() {
+    function setIngredientTable() {
         const index = 0;
-        setProducts(products => ([...products, { id: index, name: "Chicken", expiration: "eternal", delete: <BsFillTrashFill onClick={(event) => deleteIngredient(event, index)}/>}]))
+        setIngredientNames(products => ([...products, { id: index, name: "Chicken", expiration: "eternal", delete: <BsFillTrashFill onClick={(event) => deleteIngredient(event, index)}/>}]))
     }
 
     function deleteIngredient(e, index) {
         e.preventDefault();
 
-        let copy = products.filter(item => item.id !== index)
-
+        let copy = ingredientNames.filter(item => item.id !== index)
         for (let i = 0; i < copy.length; i++) {
             if (copy[i].id > index) {
                 copy[i].id--;
             }
         }
-        setProducts(copy);
+        setIngredientTable(copy);
     }
 
     const columns = [
@@ -78,12 +125,12 @@ export default function MyPantryView() {
                     <BootstrapTable
                         bootstrap4
                         keyField="id"
-                        data={products}
+                        data={ingredientNames}
                         columns={columns}
                     />
                 </div>
-                <button onClick={setIngredients}>Remove this button after add ingredient is implemented</button>
-                Note: deleting one ingredient will delete them all since they are all currently created with the same ID
+                {/*<button onClick={setIngredientNames}>Remove this button after add ingredient is implemented</button>*/}
+                {/*Note: deleting one ingredient will delete them all since they are all currently created with the same ID*/}
             </div>
         </div>
     );
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -8,6 +8,7 @@ export default function NavigationBar() {
 				<Link to="/" className='homeButton' style={{textDecoration: 'none'}}>Appetyte</Link>
 				<span className='rightJustified'>
                         <label className='navigationBarText'>Cookbook</label>
+						<Link to="/create_recipe" className='navigationBarText'>{" Create Recipe "}</Link>
                         <Link to="/pantry" className='navigationBarText'>{" My Pantry "}</Link>
                         <Link to="/profile" className='navigationBarText'>{" Profile "}</Link>
                         <Link to="/login" className='navigationBarText'>{" Login"}</Link>
