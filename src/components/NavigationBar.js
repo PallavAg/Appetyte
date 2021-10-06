d946ea4de3be3b5567f2e42851fd68b5a99fd93d
Pallav Agarwal <agpallav@gmail.com>
2021-10-06 01:27:54 -0400

    Changed Navbar to Bootstrap

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,21 +1,22 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {useAuth} from "../contexts/AuthContext";
+import {Nav, Navbar} from "react-bootstrap";
 
 export default function NavigationBar() {
 	const {uid} = useAuth()
 
 	return (
-		<div className='navigationBar' >
-			<div className='navigationBarContent'>
-				<Link to="/" className='homeButton' style={{textDecoration: 'none'}}>Appetyte</Link>
-				<span className='rightJustified'>
-                        <label className='navigationBarText'>Cookbook</label>
-                        <Link to="/pantry" className='navigationBarText'>{" My Pantry "}</Link>
-                        <Link to="/profile" className='navigationBarText'>{" Profile "}</Link>
-                        <Link to="/login" className='navigationBarText'>{uid ? "Logout" : "Login"}</Link>
-                    </span>
-			</div>
+		<div>
+			<Navbar variant="dark" className="navbar navigationBar navigationBarContent py-3">
+				<Navbar.Brand as={Link} to="/" className="navbar-brand homeButton">Appetyte</Navbar.Brand>
+				<Nav className="me-auto">
+					<Nav.Link as={Link} to="/ingredients">Ingredients</Nav.Link>
+					<Nav.Link as={Link} to="/pantry">My Pantry</Nav.Link>
+					<Nav.Link as={Link} to="/profile">{uid ? " Profile " : ""}</Nav.Link>
+					<Nav.Link as={Link} to="/login">{uid ? "" : "Login"}</Nav.Link>
+				</Nav>
+			</Navbar>
 		</div>
 	);
 }
