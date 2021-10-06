0f7847eacca2dcd6b4fe295343bd8667c902d2db
Pallav Agarwal <agpallav@gmail.com>
2021-10-06 12:25:42 -0400

    Adds confirm password field to signup

--- src/components/SignUpView.js
+++ src/components/SignUpView.js
@@ -7,10 +7,11 @@ import {toast} from "react-hot-toast";
 export default function SignUp() {
 	const emailRef = React.createRef()
 	const passwordRef = useRef()
+	const passwordConfRef = useRef()
 	const {signup, login, logout} = useAuth() // Access to Auth functions and variables
 	const [error, setError] = useState("")
 	const [loading, setLoading] = useState(false)
-	const [loggedIn, setloggedIn] = useState(false)
+	const [loggedIn, setLoggedIn] = useState(false)
 	const [signingIn, setSigningIn] = useState(true)
 	const history = useHistory()
 
@@ -24,6 +25,7 @@ export default function SignUp() {
 		e.preventDefault() // Prevents refresh
 
 		const password = passwordRef.current.value
+		const passwordConfirm = passwordConfRef.current?.value
 		const email = emailRef.current.value
 
 		setError('')
@@ -34,6 +36,11 @@ export default function SignUp() {
 				if (password.length < 6) {
 					setLoading(false)
 					return setError('Password must be at least 6 characters long')
+				}
+
+				if (password !== passwordConfirm) {
+					setLoading(false)
+					return setError('Passwords do not match. Type it again.')
 				}
 
 				// Attempt Sign Up
@@ -55,7 +62,7 @@ export default function SignUp() {
 				// Log In (located in auth context)
 				await login(email, password).then((user) => {
 					if (user.user.emailVerified) {
-						setloggedIn(true)
+						setLoggedIn(true)
 						toast.success("Logged In Successfully!")
 					} else {
 						logout()
@@ -71,23 +78,37 @@ export default function SignUp() {
 		setLoading(false)
 	}
 
+	function ConfirmPassword() {
+		if (signingIn) {
+			return (
+				<Form.Group id="password-confirm" style={{padding: 5}}>
+					<Form.Label>Confirm Password</Form.Label>
+					<Form.Control type="password" ref={passwordConfRef} required/>
+				</Form.Group>
+			)
+		} else {
+			return (<></>)
+		}
+	}
+
 	return (
 		<>
-			<Container className="d-flex align-items-center justify-content-center" style={{minHeight: "40vh"}}>
+			<Container className="d-flex align-items-center justify-content-center" style={{padding: 30}}>
 				<div className="w-100" style={{maxWidth: "400px"}}>
 					<Card>
 						<Card.Body>
 							<h2 className="text-center mb-4">{signingIn ? "Sign Up" : "Log In"}</h2>
 							{error && <Alert variant="danger">{error}</Alert>}
 							<Form onSubmit={handleSubmit}>
-								<Form.Group id="email">
+								<Form.Group id="email" style={{padding: 5}}>
 									<Form.Label>Email</Form.Label>
 									<Form.Control type="email" ref={emailRef} required/>
 								</Form.Group>
-								<Form.Group id="password">
+								<Form.Group id="password" style={{padding: 5}}>
 									<Form.Label>Password</Form.Label>
 									<Form.Control type="password" ref={passwordRef} required/>
 								</Form.Group>
+								<ConfirmPassword/>
 								<br/>
 								<Button disabled={loading} className="w-100" type="submit">
 									{signingIn ? "Sign Up" : "Log In"}
@@ -96,7 +117,7 @@ export default function SignUp() {
 						</Card.Body>
 					</Card>
 					<div className="w-100 text-center mt-2">
-						<Button type="button" className="btn-light btn-link" onClick={() => {
+						<Button type="button" className="btn-light btn-link shadow-none" onClick={() => {
 							setSigningIn(!signingIn); setError('')
 						}}>{signingIn ? "Already have an account? Login instead." : "Create an Account"}</Button>
 					</div>
