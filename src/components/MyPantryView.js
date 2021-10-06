079461434553b5554ba292335fff01fc53cd6049
Tom Yarrow <tyarrow1@gmail.com>
2021-10-06 00:30:29 -0400

    Added ingredient UI and fixed deletion inconsistencies

--- src/components/MyPantryView.js
+++ src/components/MyPantryView.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
-import { Button } from 'react-bootstrap';
+import {Button, Form, Row, Col} from "react-bootstrap";
 import { BsFillTrashFill } from 'react-icons/bs';
 import firebase, {auth, db} from "../firebase";
 import {useAuth} from "../contexts/AuthContext";
@@ -11,47 +11,69 @@ import {useAuth} from "../contexts/AuthContext";
 export default function MyPantryView() {
     const {uid} = useAuth();
     const [error, setError] = useState("");
+    const [products, setProducts] = useState([]);
+    const [futureIngredient, setFutureIngredient] = useState({name: "", expiration: ""});
+
+    useEffect(()=>{
+        getIngredients();
+    }, []);
+
+    function setField(field, value) {
+        let copyIngredient = futureIngredient;
+        if (field === "name") {
+            copyIngredient.name = value;
+        } else if (field === "expiration") {
+            copyIngredient.expiration = value;
+        }
+        setFutureIngredient(copyIngredient);
+    }
 
     function getIngredients() {
+        db.collection("Users").doc(uid).collection("Pantry").get().then(ingredients => updateIngredients(ingredients.docs));
         try {
             let userId = auth.tenantId;
-            console.log(userId);
-            db.collection("Users").doc(uid).collection("Pantry").get().then(ingredients => updateIngredients(ingredients));
+
         } catch (err) {
             console.log(err.message)
         }
-
     }
 
-    const [products, setProducts] = useState([]);
 
     function addIngredient(e, name, expiration) {
         e.preventDefault();
-        if (name ==="" || expiration === "") {
+        if (name === "") {
+            setError("Your ingredient needs a name!");
             return;
         }
+        if (expiration === "") {
+            setError("Your ingredient needs an expiration!");
+            return;
+        }
+        setError("");
 
         // DB code
         let docData = {expiration: expiration};
-        db.collection("Users").doc(uid).collection("Pantry").doc(name).set(docData).then(getIngredients);
+        db.collection("Users").doc(uid).collection("Pantry").doc(name).set(docData).then(getIngredients, onError);
+    }
+
+    function onError(e) {
+        setError(e.message);
     }
 
     function updateIngredients(ingredients) {
         let newIngredients = [];
-        let i=0;
-        ingredients.forEach( ingredient => {
-                console.log(ingredient)
-                newIngredients.push(
-                    {
-                        id: i,
-                        name: ingredient.id,
-                        expiration: ingredient.data().expiration,
-                        delete: <BsFillTrashFill onClick={(event) => deleteIngredient(event, i++)}/>
-                    }
-                )
+        ingredients.forEach( (ingredient, index) => {
+            newIngredients.push(
+                {
+                    id: index,
+                    name: ingredient.id,
+                    expiration: ingredient.data().expiration,
+                    delete: <BsFillTrashFill onClick={(event) => deleteIngredient(event, ingredient.id)}/>
+                }
+            );
             }
         );
-        console.log(ingredients);
+        console.log("Updating Ingredients");
         setProducts(newIngredients);
     }
 
@@ -61,19 +83,13 @@ export default function MyPantryView() {
         //setProducts(products => ([...products, { id: index, name: "Chicken", expiration: "eternal", delete: <BsFillTrashFill onClick={(event) => deleteIngredient(event, index)}/>}]))
     }
 
-    function deleteIngredient(e, index) {
+    function deleteIngredient(e, id) {
         e.preventDefault();
-        let ingredient = products.find(item => item.id === index)
-        db.collection("Users").doc(uid).collection("Pantry").doc(ingredient.name).delete().then(getIngredients);
-
-        // let copy = products.filter(item => item.id !== index)
-        //
-        // for (let i = 0; i < copy.length; i++) {
-        //     if (copy[i].id > index) {
-        //         copy[i].id--;
-        //     }
-        // }
-        // setProducts(copy);
+        //let ingredient = products[index];
+        // let ingredient = products.find(item => item.id === index);
+        // console.log(products);
+        // console.log(ingredient);
+        db.collection("Users").doc(uid).collection("Pantry").doc(id).delete().then(getIngredients);
     }
 
     const columns = [
@@ -105,9 +121,34 @@ export default function MyPantryView() {
                 <div className='pageTitle'>
                     My Pantry
                     <span style={{padding: 10}} /* This is blocking the Nav Bar Buttons */ />
-                    <button onClick={(event) => addIngredient(event, "Apple", "Yes")}>+</button>
+
                 </div>
                 <div style={{color: 'red', paddingTop: '1rem', fontSize: 17}}>{error}</div>
+                <div>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Row>
+                                <Col sm={5}>
+                                    <Form.Control
+                                        type="name"
+                                        placeholder={"Ingredient"}
+                                        onChange={e => setField('name', e.target.value)}
+                                    />
+                                </Col>
+                                <Col sm={5}>
+                                    <Form.Control
+                                        type="expiration"
+                                        placeholder={"Expiration"}
+                                        onChange={e => setField('expiration', e.target.value)}
+                                    />
+                                </Col>
+                                <Col>
+                                    <button onClick={(event) => addIngredient(event, futureIngredient.name, futureIngredient.expiration)}>+</button>
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                    </Form>
+                </div>
                 <div>
                     <BootstrapTable
                         bootstrap4
@@ -116,8 +157,8 @@ export default function MyPantryView() {
                         columns={columns}
                     />
                 </div>
-                <button onClick={setIngredients}>Remove this button after add ingredient is implemented</button>
-                Note: deleting one ingredient will delete them all since they are all currently created with the same ID
+                {/*<button onClick={setIngredients}>Remove this button after add ingredient is implemented</button>*/}
+                {/*Note: deleting one ingredient will delete them all since they are all currently created with the same ID*/}
             </div>
         </div>
     );
